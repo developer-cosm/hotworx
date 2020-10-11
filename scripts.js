c6d89dea8a218fd9e89ba906361782c3b59d43ba
mahmud <mahmud.linked@gmail.com>
2020-10-11 18:07:58 +0600

    First commit

---
+++ scripts.js
@@ -0,0 +1,115 @@
+jQuery(document).ready(function(){
+
+  $('#contact-state').append(`<option rel="US" value="AL" >Alabama</option>
+<option rel="US" value="AK" >Alaska</option>
+<option rel="US" value="AZ" >Arizona</option>
+<option rel="US" value="AR" >Arkansas</option>
+<option rel="US" value="CA" >California</option>
+<option rel="US" value="CO" >Colorado</option>
+<option rel="US" value="CT" >Connecticut</option>
+<option rel="US" value="DE" >Delaware</option>
+<option rel="US" value="FL" >Florida</option>
+<option rel="US" value="GA" >Georgia</option>
+<option rel="US" value="HI" >Hawaii</option>
+<option rel="US" value="ID" >Idaho</option>
+<option rel="US" value="IL" >Illinois</option>
+<option rel="US" value="IN" >Indiana</option>
+<option rel="US" value="IA" >Iowa</option>
+<option rel="US" value="KS" >Kansas</option>
+<option rel="US" value="KY" >Kentucky</option>
+<option rel="US" value="LA" >Louisiana</option>
+<option rel="US" value="ME" >Maine</option>
+<option rel="US" value="MD" >Maryland</option>
+<option rel="US" value="MA" >Massachusetts</option>
+<option rel="US" value="MI" >Michigan</option>
+<option rel="US" value="MN" >Minnesota</option>
+<option rel="US" value="MS" >Mississippi</option>
+<option rel="US" value="MO" >Missouri</option>
+<option rel="US" value="MT" >Montana</option>
+<option rel="US" value="NE" >Nebraska</option>
+<option rel="US" value="NV" >Nevada</option>
+<option rel="US" value="NH" >New Hampshire</option>
+<option rel="US" value="NJ" >New Jersey</option>
+<option rel="US" value="NM" >New Mexico</option>
+<option rel="US" value="NY" >New York</option>
+<option rel="US" value="NC" >North Carolina</option>
+<option rel="US" value="ND" >North Dakota</option>
+<option rel="US" value="OH" >Ohio</option>
+<option rel="US" value="OK" >Oklahoma</option>
+<option rel="US" value="OR" >Oregon</option>
+<option rel="US" value="PA" >Pennsylvania</option>
+<option rel="US" value="RI" >Rhode Island</option>
+<option rel="US" value="SC" >South Carolina</option>
+<option rel="US" value="SD" >South Dakota</option>
+<option rel="US" value="TN" >Tennessee</option>
+<option rel="US" value="TX" >Texas</option>
+<option rel="US" value="UT" >Utah</option>
+<option rel="US" value="VT" >Vermont</option>
+<option rel="US" value="VA" >Virginia</option>
+<option rel="US" value="WA" >Washington</option>
+<option rel="US" value="WV" >West Virginia</option>
+<option rel="US" value="WI" >Wisconsin</option>
+<option rel="US" value="WY" >Wyoming</option>
+<option rel="AU" value="VIC" >Victoria</option>
+<option rel="IR" value="GY" >Galway</option>`);
+
+  
+   	$('#location-search-state').append(`<option rel="US" value="AL" >Alabama</option>
+      <option rel="US" value="AK" >Alaska</option>
+      <option rel="US" value="AZ" >Arizona</option>
+      <option rel="US" value="AR" >Arkansas</option>
+      <option rel="US" value="CA" >California</option>
+      <option rel="US" value="CO" >Colorado</option>
+      <option rel="US" value="CT" >Connecticut</option>
+      <option rel="US" value="DE" >Delaware</option>
+      <option rel="US" value="FL" >Florida</option>
+      <option rel="US" value="GA" >Georgia</option>
+      <option rel="US" value="HI" >Hawaii</option>
+      <option rel="US" value="ID" >Idaho</option>
+      <option rel="US" value="IL" >Illinois</option>
+      <option rel="US" value="IN" >Indiana</option>
+      <option rel="US" value="IA" >Iowa</option>
+      <option rel="US" value="KS" >Kansas</option>
+      <option rel="US" value="KY" >Kentucky</option>
+      <option rel="US" value="LA" >Louisiana</option>
+      <option rel="US" value="ME" >Maine</option>
+      <option rel="US" value="MD" >Maryland</option>
+      <option rel="US" value="MA" >Massachusetts</option>
+      <option rel="US" value="MI" >Michigan</option>
+      <option rel="US" value="MN" >Minnesota</option>
+      <option rel="US" value="MS" >Mississippi</option>
+      <option rel="US" value="MO" >Missouri</option>
+      <option rel="US" value="MT" >Montana</option>
+      <option rel="US" value="NE" >Nebraska</option>
+      <option rel="US" value="NV" >Nevada</option>
+      <option rel="US" value="NH" >New Hampshire</option>
+      <option rel="US" value="NJ" >New Jersey</option>
+      <option rel="US" value="NM" >New Mexico</option>
+      <option rel="US" value="NY" >New York</option>
+      <option rel="US" value="NC" >North Carolina</option>
+      <option rel="US" value="ND" >North Dakota</option>
+      <option rel="US" value="OH" >Ohio</option>
+      <option rel="US" value="OK" >Oklahoma</option>
+      <option rel="US" value="OR" >Oregon</option>
+      <option rel="US" value="PA" >Pennsylvania</option>
+      <option rel="US" value="RI" >Rhode Island</option>
+      <option rel="US" value="SC" > South Carolina</option>
+      <option rel="US" value="SD" >South Dakota</option>
+      <option rel="US" value="TN" >Tennessee</option>
+      <option rel="US" value="TX" >Texas</option>
+      <option rel="US" value="UT" >Utah</option>
+      <option rel="US" value="VT" >Vermont</option>
+      <option rel="US" value="VA" >Virginia</option>
+      <option rel="US" value="WA" >Washington</option>
+      <option rel="US" value="WV" >West Virginia</option>
+      <option rel="US" value="WI" >Wisconsin</option>
+      <option rel="US" value="WY" >Wyoming</option>
+      <option rel="AU" value="VIC" >Victoria</option>
+      <option rel="IR" value="GY" >Galway</option>`);   
+  
+  
+  
+  
+  
+})// Document ready
+
